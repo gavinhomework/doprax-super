92a10c6c0a2517ad015d2fdea1320ee23390fd2f
gavinhomework <92965736+gavinhomework@users.noreply.github.com>
2023-01-31 16:08:11 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ apt update && apt install -y supervisor wget unzip iproute2
 wget -O m.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
 unzip m.zip && rm -f m.zip
 chmod a+x xray
-sed -i "s/uuid/$uuid/g" ./config.yaml
+sed -i "s/uuid/$uuid/g" ./config.json
 sed -i "s/uuid/$uuid/g" /etc/nginx/nginx.conf
 #[ -n "${www}" ] && rm -rf /usr/share/nginx/* && wget -c -P /usr/share/nginx "https://github.com/yonggekkk/doprax-xray/raw/main/3w/html${www}.zip" && unzip -o "/usr/share/nginx/html${www}.zip" -d /usr/share/nginx/html
 xpid=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
